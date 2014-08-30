dc0f8ca4df8a8a7e2a4007d66f64959f93b4046d
atash55 <atash125@yahoo.co.in>
2014-08-30 21:58:13 +0530

    jhggyu

---
+++ Climber/Assets/Scripts/UI/UI.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour
+{
+	public Animation greenWinsAnim;
+	public Animation blueWinsAnim;
+
+	public Animation gameOverScreenAnim;
+
+	static UI inst;
+
+	void Awake ()
+	{
+		inst = this;
+		//ShowWinScreen (true);
+	}
+	
+
+	public static void ShowWinScreen (bool greenWins)
+	{
+		inst.StartCoroutine( inst._ShowWinScreen (greenWins) );
+	}
+	IEnumerator _ShowWinScreen (bool greenWins)
+	{
+		Animation anim = greenWins ? greenWinsAnim : blueWinsAnim;
+		anim.Play();
+		yield return new WaitForSeconds (4);
+		foreach( AnimationState s in anim )
+			s.speed = -1;
+		anim.Play ();
+
+
+	}
+
+	void RestartLevel()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	void LoadMainMenu()
+	{
+		Application.LoadLevel (0);
+	}
+
+	// Play
+	// Help screen and back
+
+}
---
+++ Climber/Assets/WinLoseDetector.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinLoseDetector : MonoBehaviour
+{
+	public CollisionDetector deathZoneCollider;
+	public CollisionDetector summitZoneCollider;
+
+	void OnEnable()
+	{
+		deathZoneCollider.onTriggerEnter2D += EnteredDeathZone;
+		//summitZoneCollider.onTriggerEnter2D += EnteredSummitZone;
+	}
+
+	void EnteredDeathZone( Collider2D other )
+	{
+		if( other.transform.parent.name == "Climber1" )
+		{
+			Climber.DisableScript();
+			UI.ShowWinScreen(false);
+		}
+		else if( other.transform.parent.name == "Climber2" )
+		{
+			Climber.DisableScript();
+			UI.ShowWinScreen(true);
+		}
+	}
+	
+	void EnteredSummitZone( Collider2D other )
+	{
+		if( other.name == "Climber1" )
+		{
+			Climber.DisableScript();
+			UI.ShowWinScreen(true);
+		}
+		else if( other.name == "Climber2" )
+		{
+			Climber.DisableScript();
+			UI.ShowWinScreen(false);
+		}
+	}
+}
